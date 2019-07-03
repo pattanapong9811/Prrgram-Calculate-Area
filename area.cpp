c87387ab88c417e397a6b108057b033ef24803d2
Mr.aum <52208551+pattanapong9811@users.noreply.github.com>
2019-07-03 10:20:17 +0700

    Add files via upload

---
+++ area.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	
+	int length,widht;
+	float  radius, r = 3.1415;
+	cout << "Program Calculate Area Rectangle." << endl;
+	cout << "Enter Length : ";
+	cin >> length ;
+	cout << "Enter Widht : ";
+	cin >> widht ;
+	cout << "Area of Rectagle = " << length * widht << endl;
+	cout <<"========================================================" << endl;
+	cout << "Program Calculate Area Circle."<<endl;
+	cout << "Circle radius (real number) ? " ;
+	cin >> radius ;
+	cout << "Area of circle with radius 5.1 is " << radius*radius*r <<endl;
+	cout << "Circumference is " << 2*r*radius << endl;
+	return(0);
+}
